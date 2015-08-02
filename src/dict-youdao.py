e7a16264a1c947da45ff45c55fa45b619fd4988a
Guangmu Zhu <guangmuzhu@gmail.com>
2015-08-03 02:19:43 +0800

    add License and add backup file to .gitignore

--- src/dict-youdao.py
+++ src/dict-youdao.py
@@ -1,6 +1,23 @@
 #! /usr/bin/python
 # -*-  coding=utf8  -*-
 
+#    Copyright (C) 2015 Guangmu Zhu <guangmuzhu@gmail.com>
+#
+#    This file is part of GoldenDict-Dictionary-Programs.
+#
+#    GoldenDict-Dictionary-Programs is free software: you can redistribute it
+#    and/or modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    GoldenDict-Dictionary-Programs is distributed in the hope that
+#    it will be useful, but WITHOUT ANY WARRANTY; without even
+#    the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#    See the GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PyChat.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import urllib
 import urllib2
